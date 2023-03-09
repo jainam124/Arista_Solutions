ca33f77d2cd3fe4a611bc23ad65a7d4669176d87
Ved <vedbulsara7@gmail.com>
2023-03-09 19:13:17 +0530

    5th commit(9/3/23)

--- login/app.js
+++ login/app.js
@@ -4,11 +4,14 @@ const app = express();
 const path = require("path");
 const router = express.Router();
 
-router.get("/", function(req, res){
+app.use(express.static("public"))
+
+app.get("/", function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
+    res.send('<img src = "/images/arista_logo.jpg">')
 })
 
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 8000);
 
-console.log("Application running on port 3000");
+console.log("Application running on port 8000");

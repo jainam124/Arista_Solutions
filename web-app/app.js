6c5befdb82e4d854b33b86d47d61e7cedf4821d0
Ved <vedbulsara7@gmail.com>
2023-04-14 22:00:18 +0530

    19th commit (14/4/23)

--- web-app/app.js
+++ web-app/app.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const app = express();
-
-const path = require("path");
+const session = require('express-session');
 const router = express.Router();
+const mysql = require("mysql")
 
+// 8.0.32-0ubuntu0.22.04.2
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------------------------------------------- //
+// //Register
+// app.post('/register', (req, res) => {
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   pool.getConnection((err, connection) => {
+//     if (err) throw err;
+//     connection.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err, result) => {
+//       connection.release();
+//       if (err) throw err;
+//       console.log('User registered');
+//       res.send('User registered');
+//     });
+//   });
+// });
+
+
+// ----------------------------------------------------------------------------------- //
 app.use(express.static("public"))
-
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
     res.send('<img src = "/images/arista_logo.jpg">')
